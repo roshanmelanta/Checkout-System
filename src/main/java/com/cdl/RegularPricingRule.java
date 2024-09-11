4ee278fdc3e56b3530464230bebe6d7b3150e866
roshanmelanta <rmelanta255@gmail.com>
2024-09-11 21:50:30 +0100

    Set up initial structure for pricing rules with placeholder implementations

--- src/main/java/com/cdl/RegularPricingRule.java
+++ src/main/java/com/cdl/RegularPricingRule.java
@@ -1,4 +1,18 @@
 package com.cdl;
 
-public class RegularPricingRule {
+import java.math.BigDecimal;
+
+/**
+ * Implementation of PricingRule for regular pricing.
+ */
+public class RegularPricingRule implements PricingRule{
+    private final BigDecimal unitPrice;
+    public RegularPricingRule(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public BigDecimal calculatePrice(int quantity) {
+        return null;
+    }
 }
--- src/main/java/com/cdl/SpecialPricingRule.java
+++ src/main/java/com/cdl/SpecialPricingRule.java
@@ -1,4 +1,23 @@
 package com.cdl;
 
-public class SpecialPricingRule {
+import java.math.BigDecimal;
+
+/**
+ * Implementation of PricingRule for special bulk pricing.
+ */
+public class SpecialPricingRule implements PricingRule {
+    private final BigDecimal unitPrice;
+    private final int specialQuantity;
+    private final BigDecimal specialPrice;
+
+    public SpecialPricingRule(BigDecimal unitPrice, int specialQuantity, BigDecimal specialPrice) {
+        this.unitPrice = unitPrice;
+        this.specialQuantity = specialQuantity;
+        this.specialPrice = specialPrice;
+    }
+
+    @Override
+    public BigDecimal calculatePrice(int quantity) {
+        return null;
+    }
 }
